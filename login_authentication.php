a6115ac2e58abe2f61b611c83aa11f387f481b0c
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-01-10 14:21:33 +0800

    login with authentication

--- login_authentication.php
+++ login_authentication.php
@@ -4,36 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN PAGE</title>
+    <title>Document</title>
 </head>
 
 <body>
 
 <?php  
+
  $servername = "localhost";
  $username = "root";
  $password = "";
  $dbname = " ";
 
-//creating connection
-$connection = new mysqli($servername,$username,$password, $dbname);
- 
-//checking connection
-if($connection->connect_error)
-{
-	die("Connection Failed: ". $connection->connect_error);
-}	
+ include('connectiondb.php')
 
 //server side validation
  if ($_SERVER["REQUEST_METHOD"] == "POST") 
  {
- 	 if (empty($_POST["email"])) 
+ 	 if (empty($_POST["username"])) 
  	 {
-        $email = "Name is required";
+        $username = "Name is required";
      }
      else 
      {
-       $email = test_input($_POST["email"]);
+       $username = test_input($_POST["username"]);
      }
             
      if (empty($_POST["psword"])) 
@@ -44,21 +38,15 @@ if($connection->connect_error)
      else 
      {
        $psword = test_input($_POST["psword"]);
-               
-       // check if e-mail address is well-formed
-       if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
-       {
-       	$email = "Invalid email format"; 
-       }
 
 	} 
 	
 
-     if(!empty($_POST['email']) && isset($_POST['email']) && !empty($_POST['psword']) && isset($_POST['psword']))
+     if(!empty($_POST['username']) && isset($_POST['username']) && !empty($_POST['psword']) && isset($_POST['psword']))
     {
     	
-    	//checks if email and password is already been in mysql
- 		$sql = "SELECT customer_id,customer_email, customer_password FROM customer WHERE customer_email = '$email' AND customer_password = '$psword'";
+    	//checks if username and password is already been in mysql
+ 		//$sql = "SELECT username, password FROM user WHERE username = '$username' AND password = '$psword'";
  		$data = $connection->query($sql);
  		 //var_dump($data);
 
@@ -67,7 +55,7 @@ if($connection->connect_error)
        {
        			session_start();
        		    $row = $data->fetch_assoc();
-       		    $_SESSION["customer_id"] = $row['customer_id'];
+       		    $_SESSION["username"] = $row['psword'];
     
        		echo '<script type="text/javascript">
 				alert("Welcome!");
@@ -114,8 +102,8 @@ function test_input($data)
 	
 		<form action="<?php echo $_SERVER['PHP_SELF'];?>" method = "POST">	
 
-			<h3>lOGIN</h3>
-			<input type="email" name="email" required placeholder="Enter email" class="box">
+			<h3>LOGIN</h3>
+			<input type="text" name="username" required placeholder="Enter username" class="box">
 			<input type="password" name="psword" required placeholder="Enter password" class="box">
 			<input type="submit" name="login" class="btn" value="Login">
 			<p><a href=" ">Login</a></p>
